e313b072fd3ed8e1e70c1dba256bf66b08f24f79
Aquila <aquila-2004@yandex.ru>
2018-01-28 19:58:17 +0300

    Реализован класс Matrix N-размерности.

--- main.cpp
+++ main.cpp
@@ -1,265 +1,32 @@
 #include <cassert>
-
-#include <tuple>
-#include <type_traits>
-#include <array>
 #include <iostream>
-#include <string>
-#include <vector>
-#include <list>
-#include <vector>
-#include <map>
+#include <tuple>
 
-template<class T>
-using ColumnT_t = std::map<size_t, T>;
-template<class T>
-using RowT_t = std::map<size_t, ColumnT_t<T>>;
-
-template<class T>
-using MatrixT_t = std::map<size_t, ColumnT_t<T>>;
-
-
-template<class T>
-struct iterator_matrix
-{
-    using iterator_category = std::forward_iterator_tag;
-    using value_type = typename std::tuple<size_t,size_t, T&>;
-
-    iterator_matrix() = default;
-    iterator_matrix( const iterator_matrix& rhs ) = default;
-    iterator_matrix( MatrixT_t<T>& data, typename MatrixT_t<T>::iterator m, typename ColumnT_t<T>::iterator c )
-        : data( data ), m(m), c(c)
-    {
-    }
-
-    value_type operator*()
-    {
-        return std::make_tuple( m->first, c->first,  std::ref(c->second) );
-    }
-    value_type operator->()
-    {
-        return std::make_tuple( m->first, c->first, std::ref( c->second ) );
-    }
-    iterator_matrix& operator++()
-    {
-        ++c;
-        if( c == m->second.end() )
-        {
-            ++m;
-            if( m != data.end() )
-            {
-                c = m->second.begin();
-            }
-            else
-            {
-                c = typename ColumnT_t<T>::iterator();
-            }
-        }
-
-        return ( *this );
-    }
-
-    iterator_matrix operator++( int )
-    {
-        iterator_matrix it( data, m, c );
-        ++( *this );
-        return it;
-    }
-
-    /*iterator_matrix& operator--()
-    {        
-        ++c;
-        if( c == m->second.end() )
-        {
-            ++m;
-            if( m != data.end() )
-            {
-                c = m->second.begin();
-            }
-        }
-        return ( *this );
-    }
-
-    iterator_matrix operator--( int )
-    {
-        iterator_matrix it( ptr );
-        --( *this );
-        return it;
-    }*/
-
-    bool operator == ( const iterator_matrix& rhs ) const
-    {
-        return m == rhs.m && data.end() != m && c == rhs.c;
-    }
-
-    bool operator != ( const iterator_matrix& rhs ) const
-    {
-        return !((*this) == rhs);
-    }
-
-private:
-    MatrixT_t<T>& data;
-    typename MatrixT_t<T>::iterator m;
-    typename ColumnT_t<T>::iterator c;
-};
-
-template<class T>
-class ProxyValue
-{
-public:
-    ProxyValue( MatrixT_t<T>& data, size_t nRow, size_t nColumn, const T& default, size_t& size ) :
-        m_data(data), m_default(default), m_Row(nRow), m_Column(nColumn), m_Size(size)
-    {
-    }
-
-    ProxyValue& operator = ( const T& value )
-    {
-        if( value == m_default )
-        {
-            auto row = m_data.find( m_Row );
-            if( row != m_data.end() )
-            {
-                auto column = row->second.find( m_Column );
-                if( column != row->second.end() )
-                {
-                    row->second.erase( column );
-                    --m_Size;
-                }
-                if( row->second.empty() )
-                {
-                    m_data.erase( row );
-                }
-            }
-        }
-        else
-        {            
-            auto& col = m_data[m_Row];
-            auto prevSize = col.size();
-            col[m_Column] = value;
-            m_Size += col.size() - prevSize;
-        }
-        return ( *this );
-    }
-
-    operator T()
-    {        
-        auto row = m_data.find( m_Row );
-        if( row != m_data.end() )
-        {
-            auto column = row->second.find( m_Column );
-            if( column != row->second.end() )
-            {
-                return ( column->second );                
-            }
-        }
-        return m_default;
-    }
-
-private:
-    MatrixT_t<T>& m_data;
-    const T& m_default;
-    size_t& m_Size;
-    size_t m_Row;
-    size_t m_Column;
-};
-
-template<class T>
-class ProxyMatrix
-{
-public:
-    ProxyMatrix( MatrixT_t<T>& data, size_t nRow, const T& default, size_t& size ) 
-        : m_Data(data), m_NumRow(nRow), m_Default(default), m_Size(size)
-    {}
-
-    decltype(auto) operator[]( size_t nColumn )
-    {
-        return ProxyValue<T>( m_Data, m_NumRow, nColumn, m_Default, m_Size );
-    }
-   /* const T& operator[]( size_t nColumn ) const
-    {
-        return m_Data[m_NumRow][nColumn];
-    }*/
-private:
-    size_t m_NumRow;
-    MatrixT_t<T>& m_Data;
-    const T& m_Default;
-    size_t& m_Size;
-};
-
-template<class T>
-class Matrix
-{
-public:
-    using value_type = T;
-
-    Matrix( const T& defaultValue = T() ) 
-        : m_Default( defaultValue )
-    {
-    }
-    decltype( auto ) operator[]( size_t nRow )
-    {
-        return ProxyMatrix<T>( m_Data, nRow, m_Default, m_Size );
-    }
-    decltype( auto ) operator[]( size_t nRow ) const
-    {
-        return ProxyMatrix<T>( m_Data, nRow, m_Default, m_Size );
-    }
-
-    iterator_matrix<T> begin()
-    {
-        ColumnT_t<T>::iterator c;
-        if( m_Data.empty() == false )
-            c = m_Data.begin()->second.begin();
-        return iterator_matrix<T>( m_Data, m_Data.begin(), c );
-    }
-    iterator_matrix<T> end()
-    {
-        ColumnT_t<T>::iterator c;        
-        return iterator_matrix<T>( m_Data, m_Data.end(), c );
-    }
-
-    size_t size() const
-    {
-        return m_Size;
-    }
-private:
-  
-    std::map<size_t, std::map<size_t, T> > m_Data;
-    size_t m_Size = 0;
-    T m_Default;
-};
+#include "matrix.h"
 
 
 int main()
 {
-    Matrix<int> matrix( -1 );
+    Matrix<int> matrix(-1);
     assert( matrix.size() == 0 );
 
     auto a = matrix[0][0];
     assert( a == -1 );
 
     assert( matrix.size() == 0 );
-    matrix[100][100] = 314;
+    matrix[100][100] = 314L;
 
     assert( matrix[100][100] == 314 );
     assert( matrix.size() == 1 );
-
-    
-    for( auto c : matrix )
-    {
-        int x, y, v;
-        std::tie( x, y, v ) = c;
-        std::cout << x << ' ' << y << ' ' << v << std::endl;
-    }
 
 
     Matrix<int> mat( 0 );
     for( size_t i = 0; i < 10; ++i )
     {
         mat[i][i] = i;
-        mat[i][9 - i] = 9 - i;
+        mat[i][9 - i] = i;
     }
-    
+
     for( size_t i = 1; i < 9; ++i )
     {
         for( size_t j = 1; j < 9; ++j )
@@ -274,7 +41,7 @@ int main()
     {
         int x, y, v;
         std::tie( x, y, v ) = c;
-        std::cout << x << ' ' << y << ' ' << v << std::endl;
+        std::cout << "[ " << x << " ][ " << y << " ] = " << v << std::endl;
     }
 
     return 0;
---
+++ matrix.h
@@ -0,0 +1,222 @@
+#pragma once
+
+#include <map>
+#include <array>
+#include <tuple>
+
+namespace detail{
+
+//===========================================================/
+//  Шаблон для генерации tuple с N элементов одного типа     /         
+//===========================================================/
+template <size_t I, class T, class... P>
+struct gen_tuple
+{
+    using type = typename gen_tuple<I - 1, T, T, P...>::type;
+};
+
+template<class T, class... P>
+struct gen_tuple<0, T, P...>
+{
+    using type = std::tuple<P...>;
+};
+
+template<class T, size_t I>
+using gen_tuple_t = typename gen_tuple<I, T>::type;
+//===========================================================/
+
+/// Тип индексов
+template<size_t Size>
+using index_t = gen_tuple_t<size_t, Size>;
+
+/// Тип контейенра для хранения разряженной матрицы
+template<class Ty, size_t Size>
+using mat_t = std::map<gen_tuple_t<size_t, Size>, Ty>;
+
+/// Прокси-класс для работы со значением
+template<class Ty, size_t N>
+class ProxyValue
+{
+public:
+    ProxyValue( mat_t<Ty, N >& data, index_t<N>& indices, const Ty& default )
+        : m_data( data ), m_indices( indices ), m_default( default )
+    {}
+
+    ProxyValue& operator = ( const Ty& value )
+    {
+        if( value == m_default )
+        {
+            auto iter = m_data.find( m_indices );
+            if( iter != m_data.end() )
+            {
+                m_data.erase( iter );
+            }
+        }
+        else
+        {
+            m_data[m_indices] = value;
+        }
+        return ( *this );
+    }
+
+    operator Ty()
+    {
+        auto iter = m_data.find( m_indices );
+        if( iter != m_data.end() )
+        {
+            return iter->second;
+        }
+        return m_default;
+    }
+
+private:
+    mat_t<Ty, N>& m_data;
+    const Ty& m_default;
+    index_t<N>& m_indices;
+};
+
+/// Прокси-класс для реализация N мерного оператора []
+template<class Ty, size_t N, size_t I>
+class ProxyMatrix
+{
+public:
+    ProxyMatrix( mat_t<Ty, N>& data, index_t<N>& indices, const Ty& default )
+        : m_data( data ), m_indices( indices ), m_default( default )
+    {}
+
+    auto operator[]( size_t index )
+    {
+        std::get<I - 1>( m_indices ) = index;
+        return ProxyMatrix<Ty, N, I - 1>( m_data, m_indices, m_default );
+    }
+
+private:
+    mat_t< Ty, N >& m_data;
+    const Ty& m_default;
+    index_t< N >& m_indices;
+};
+
+template<class Ty, size_t N>
+class ProxyMatrix<Ty, N, 1>
+{
+public:
+    ProxyMatrix( mat_t<Ty, N>& data, index_t<N>& indices, const Ty& default )
+        : m_data( data ), m_indices( indices ), m_default( default )
+    {}
+
+    auto operator[]( size_t index )
+    {
+        std::get<0>( m_indices ) = index;
+        return ProxyValue<Ty, N>( m_data, m_indices, m_default );
+    }
+
+private:
+    mat_t<Ty, N>& m_data;
+    const Ty& m_default;
+    index_t<N>& m_indices;
+};
+
+} // namespace detail
+
+/// Итератор
+template<class T, size_t N>
+struct iterator_matrix
+{
+    using iterator_category = typename detail::mat_t<T, N>::iterator::iterator_category;
+
+    iterator_matrix() = default;
+    iterator_matrix( const iterator_matrix& rhs ) = default;
+    iterator_matrix( typename detail::mat_t<T, N>::iterator iter )
+        : m_iter( iter )
+    {
+    }
+
+    auto operator*()
+    {
+        return std::tuple_cat( m_iter->first, std::make_tuple( std::ref( m_iter->second ) ) );
+    }
+
+    auto operator->()
+    {
+        return std::tuple_cat( m_iter->first, std::make_tuple( std::ref( m_iter->second ) ) );
+    }
+
+    iterator_matrix& operator++()
+    {
+        ++m_iter;
+        return ( *this );
+    }
+
+    iterator_matrix operator++( int )
+    {
+        iterator_matrix it( m_iter );
+        ++( *this );
+        return it;
+    }
+
+    iterator_matrix& operator--()
+    {
+        --m_iter;
+        return ( *this );
+    }
+
+    iterator_matrix operator--( int )
+    {
+        iterator_matrix it( m_iter );
+        --( *this );
+        return it;
+    }
+
+    bool operator == ( const iterator_matrix& rhs ) const
+    {
+        return m_iter == rhs.m_iter;
+    }
+
+    bool operator != ( const iterator_matrix& rhs ) const
+    {
+        return !( ( *this ) == rhs );
+    }
+
+private:
+    typename detail::mat_t<T, N>::iterator m_iter;
+};
+
+/// Класс матрицы
+template<class Ty, size_t N = 2>
+class Matrix
+{
+public:
+    using value_type = Ty;
+    using iterator = iterator_matrix<Ty, N>;
+
+    Matrix( const Ty& default_value = Ty() )
+        : m_default( default_value )
+    {
+    }
+
+    auto operator[]( size_t index )
+    {
+        std::get<N - 1>( m_need_index ) = index;
+        return detail::ProxyMatrix<Ty, N, N - 1>( m_data, m_need_index, m_default );
+    }
+
+    iterator begin()
+    {
+        return iterator( m_data.begin() );
+    }
+
+    iterator end()
+    {
+        return iterator( m_data.end() );
+    }
+
+    size_t size() const
+    {
+        return m_data.size();
+    }
+    
+private:
+    detail::index_t<N> m_need_index;
+    detail::mat_t<Ty, N> m_data;
+    Ty m_default;
+};
